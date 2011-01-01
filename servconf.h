91436fe9d04e0a47db88386ccea0a21b42e59112
Simon Wilkinson <sxw@inf.ed.ac.uk>
2011-01-01 20:45:31 +0000

    Add the GSSAPIStrictAcceptorCheck option
    Add a configuration hook to control whether we check that the
    acceptor identity matches host/gethostbyname() or whether we
    permit any acceptor contained in the default keytab.

--- servconf.h
+++ servconf.h
@@ -95,6 +95,7 @@ typedef struct {
 						 * authenticated with Kerberos. */
 	int     gss_authentication;	/* If true, permit GSSAPI authentication */
 	int     gss_cleanup_creds;	/* If true, destroy cred cache on logout */
+	int 	gss_strict_acceptor;	/* If true, restrict the GSSAPI acceptor name */
 	int     password_authentication;	/* If true, permit password
 						 * authentication. */
 	int     kbd_interactive_authentication;	/* If true, permit */
